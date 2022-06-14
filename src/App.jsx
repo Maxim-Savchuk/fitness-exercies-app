8d175d3dc89dc828f598a9b0b3255bc24fd6522f
maxim-savchuk <maxsavchuk1105@gmail.com>
2022-06-14 17:29:03 +0300

    add dependencies, base files and folder structure

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { Navbar, Footer } from './components';
+import { Home, ExerciseDetails } from './pages';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetails />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
+};
+
+export default App;
