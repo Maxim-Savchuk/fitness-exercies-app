adb019798d3781b756f31306b40a32f7872b2e13
maxim-savchuk <maxsavchuk1105@gmail.com>
2022-06-16 17:53:20 +0300

    Finish SearchExercises
    Add SearchExercises component, Fetching data, Horizontal scrollbar for categories and BodyPart component

---
+++ src/components/BodyPart.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Stack, Typography } from '@mui/material';
+
+import Icon from '../assets/icons/gym.png';
+
+const BodyPart = ({ item, bodyPart, setBodyPart }) => {
+    return (
+        <Stack className='bodyPart-card'
+            type="button"
+            alignItems="center"
+            justifyContent="center"
+            sx={{
+                borderTop: bodyPart === item ? "4px solid #FF2625" : '',
+                backgroundColor: "#FFF",
+                borderBottomLeftRadius: '20px',
+                width: '270px',
+                height: '270px',
+                cursor: 'pointer',
+                gap: '47px'
+            }}
+            onClick={() => {
+                setBodyPart(item)
+                window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' })
+            }}
+        >
+            <img src={Icon} alt="dumbbell" style={{ width: '40px', height: '40px' }} />
+            <Typography fontSize="24px" fontWeight="bold" color="#3A1212" textTransform='capitalize'>{item}</Typography>
+        </Stack >
+    )
+}
+
+export default BodyPart;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,14 @@ import { Box } from '@mui/material';
 import { HeroBanner, SearchExercises, Exercises } from '../components'
 
 const Home = () => {
+    const [bodyPart, setBodyPart] = useState('all')
+    const [exercises, setExercises] = useState([]);
+
     return (
         <Box>
             <HeroBanner />
-            <SearchExercises />
-            <Exercises />
+            <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+            <Exercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
         </Box>
     )
 }
