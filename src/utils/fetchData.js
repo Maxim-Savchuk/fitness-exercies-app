70ca3fa88069960f7cdc00236a30ee4f6ffe04be
maxim-savchuk <maxsavchuk1105@gmail.com>
2022-06-21 16:42:38 +0300

    Finish Exercises component

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -7,7 +7,11 @@ export const exerciseOptions = {
 };
 
 export const fetchData = async (url, options) => {
-  const response = await fetch(url, options);
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
